9f3744b48d5318d4421a9be5e4d1514bea467646
Ari Wiradana <ariwiradana5@gmail.com>
2023-07-16 15:11:55 +0800

    sds

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ connectDB();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 require("./routes")(app);
 
 const PORT = process.env.PORT;
